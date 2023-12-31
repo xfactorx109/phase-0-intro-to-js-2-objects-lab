8d465428b09d30ee32e307442112ab0047dd430c
Xavier Humes-Sanchez <xfactorx109@gmail.com>
2023-12-30 21:50:23 -0800

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Sam',
+    age: 30,
+    department: 'Engineering'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = Object. assign({}, employee);
+    newEmployee[key] = value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newEmployee = Object.assign({}, employee);
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key];
+    return employee;
+}
